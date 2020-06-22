79acafe8aee0e93ab2713cea76f9acb98a525900
eechava6 <eechava6@eafit.edu.co>
2020-06-22 16:35:04 -0500

    Adding template

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+              <div className="author-img" style={{backgroundImage: 'url(images/sidebar.png)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">Esteban Echavarría</a></h1>
+              <span className="email"><i className="icon-mail"></i> esteban.ec123@gmail.com </span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li><a href="#about" data-nav-section="about">About</a></li>
+                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+                <li>LinkedIn <a href="https://www.linkedin.com/in/dbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" />  </a></li>
+                <li>Github <a href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i>  </a></li>
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+              <p><small>
+                  Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
+                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
+              </small></p>
+
+            </div>
+          </aside>
+        </div>
+      </div>
+    )
+  }
+}
