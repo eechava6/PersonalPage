9d5000040c782cbf1bd2db4fde571d9f303a5754
eechava6 <eechava6@eafit.edu.co>
2020-08-10 11:53:50 -0500

    Updates

---
+++ precache-manifest.fd94094ff7576d8b6db341c0af9849ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f809bb385654f967dc4d0b7dbdf97634",
+    "url": "/PersonalPage/index.html"
+  },
+  {
+    "revision": "5f9d59aef10035f0ec86",
+    "url": "/PersonalPage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/PersonalPage/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PersonalPage/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9d59aef10035f0ec86",
+    "url": "/PersonalPage/static/js/main.ae7902af.chunk.js"
+  },
+  {
+    "revision": "c436f849eb24fa02ac9b",
+    "url": "/PersonalPage/static/js/runtime-main.785b4b0f.js"
+  }
+]);
